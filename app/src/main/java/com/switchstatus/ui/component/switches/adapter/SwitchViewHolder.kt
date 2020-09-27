44ed36f0f17d509f44324718ca7ce5a6702dc189
Phuc Nguyen <thienphuc0510@gmail.com>
2020-09-27 15:10:33 +0700

    change icon switch

--- app/src/main/java/com/switchstatus/ui/component/switches/adapter/SwitchViewHolder.kt
+++ app/src/main/java/com/switchstatus/ui/component/switches/adapter/SwitchViewHolder.kt
@@ -1,9 +1,12 @@
 package com.switchstatus.ui.component.switches.adapter
 
 import androidx.recyclerview.widget.RecyclerView
+import com.github.angads25.toggle.interfaces.OnToggledListener
+import com.github.angads25.toggle.model.ToggleableView
 import com.switchstatus.data.dto.switches.ItemSwitch
 import com.switchstatus.databinding.SwitchItemBinding
 import com.switchstatus.ui.base.listeners.SwitchStatusListener
+
 
 /**
  * Created by AhmedEltaher
@@ -13,15 +16,22 @@ class SwitchViewHolder(private val itemBinding: SwitchItemBinding) : RecyclerVie
 
     fun bind(switch: ItemSwitch, switchStatusListener: SwitchStatusListener) {
         itemBinding.tvName.text = switch.displayName
-        itemBinding.switchStatus.isChecked = switch.status
-        itemBinding.switchStatus.setOnCheckedChangeListener { buttonView, isChecked ->
-            switchStatusListener.onSwitchStatusListener(switch, isChecked)
-            if (isChecked) {
-                // The switch is enabled/checked
-            } else {
-                // The switch is disabled
+        itemBinding.switchStatus.isOn = switch.status
+
+        itemBinding.switchStatus.setOnToggledListener(object : OnToggledListener {
+            override fun onSwitched(toggleableView: ToggleableView?, isOn: Boolean) {
+                switchStatusListener.onSwitchStatusListener(switch, isOn)
             }
-        }
+        })
+
+//        itemBinding.switchStatus.setOnCheckedChangeListener { buttonView, isChecked ->
+//            switchStatusListener.onSwitchStatusListener(switch, isChecked)
+//            if (isChecked) {
+//                // The switch is enabled/checked
+//            } else {
+//                // The switch is disabled
+//            }
+//        }
     }
 }
 
